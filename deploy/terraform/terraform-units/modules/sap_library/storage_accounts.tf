3b17e66270e86f2431719ae01becd7b777b61bbc
Kimmo Forss <kimforss@microsoft.com>
2024-11-13 01:22:50 +0200

    Fix casing for subresource name in SAP library storage accounts configuration

--- deploy/terraform/terraform-units/modules/sap_library/storage_accounts.tf
+++ deploy/terraform/terraform-units/modules/sap_library/storage_accounts.tf
@@ -422,7 +422,7 @@ resource "azurerm_private_endpoint" "storage_sapbits" {
                                  azurerm_storage_account.storage_sapbits[0].id
                                )
                                subresource_names = [
-                                                     "Blob"
+                                                     "blob"
                                                    ]
                              }
 
