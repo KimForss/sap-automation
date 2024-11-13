e9294e1dc75fb0f614a06c6e2fa9fedb1c4763de
Kimmo Forss <kimforss@microsoft.com>
2024-11-13 19:14:56 +0200

    Update DNS A records to use private endpoint IP addresses for improved accuracy and reliability

--- deploy/terraform/terraform-units/modules/sap_landscape/storage_accounts.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/storage_accounts.tf
@@ -45,7 +45,7 @@ resource "azurerm_private_dns_a_record" "storage_bootdiag" {
                                            azurerm_resource_group.resource_group[0].name
                                          )
   ttl                                  = 3600
-  records                              = [data.azurerm_network_interface.storage_bootdiag[count.index].ip_configuration[0].private_ip_address]
+  records                              = [azurerm_private_endpoint.storage_bootdiag[count.index].private_service_connection.private_ip_address]
   tags                                 = var.tags
 }
 
@@ -181,7 +181,7 @@ resource "azurerm_private_dns_a_record" "witness_storage" {
   zone_name                            = var.dns_settings.dns_zone_names.blob_dns_zone_name
   resource_group_name                  = var.dns_settings.privatelink_dns_resourcegroup_name
   ttl                                  = 3600
-  records                              = [data.azurerm_network_interface.witness_storage[count.index].ip_configuration[0].private_ip_address]
+  records                              = [azurerm_private_endpoint.witness_storage[count.index].private_service_connection.private_ip_address]
 
   tags                                 = var.tags
 
@@ -755,19 +755,19 @@ resource "time_sleep" "wait_for_private_endpoints" {
 }
 
 
-data "azurerm_network_interface" "storage_bootdiag" {
-  provider                             = azurerm.main
-  count                                = var.use_private_endpoint && length(var.diagnostics_storage_account.arm_id) == 0 && length(try(azurerm_private_endpoint.storage_bootdiag[0].network_interface[0].id, "")) > 0 ? 1 : 0
-  name                                 = azurerm_private_endpoint.storage_bootdiag[count.index].network_interface[0].name
-  resource_group_name                  = split("/", azurerm_private_endpoint.storage_bootdiag[count.index].network_interface[0].id)[4]
-}
+# data "azurerm_network_interface" "storage_bootdiag" {
+#   provider                             = azurerm.main
+#   count                                = var.use_private_endpoint && length(var.diagnostics_storage_account.arm_id) == 0 && length(try(azurerm_private_endpoint.storage_bootdiag[0].network_interface[0].id, "")) > 0 ? 1 : 0
+#   name                                 = azurerm_private_endpoint.storage_bootdiag[count.index].network_interface[0].name
+#   resource_group_name                  = split("/", azurerm_private_endpoint.storage_bootdiag[count.index].network_interface[0].id)[4]
+# }
 
-data "azurerm_network_interface" "witness_storage" {
-  provider                             = azurerm.main
-  count                                = var.use_private_endpoint && length(var.witness_storage_account.arm_id) == 0 && length(try(azurerm_private_endpoint.witness_storage[0].network_interface[0].id, "")) > 0 ? 1 : 0
-  name                                 = azurerm_private_endpoint.witness_storage[count.index].network_interface[0].name
-  resource_group_name                  = split("/", azurerm_private_endpoint.witness_storage[count.index].network_interface[0].id)[4]
-}
+# data "azurerm_network_interface" "witness_storage" {
+#   provider                             = azurerm.main
+#   count                                = var.use_private_endpoint && length(var.witness_storage_account.arm_id) == 0 && length(try(azurerm_private_endpoint.witness_storage[0].network_interface[0].id, "")) > 0 ? 1 : 0
+#   name                                 = azurerm_private_endpoint.witness_storage[count.index].network_interface[0].name
+#   resource_group_name                  = split("/", azurerm_private_endpoint.witness_storage[count.index].network_interface[0].id)[4]
+# }
 
 # data "azurerm_network_interface" "install" {
 #   provider            = azurerm.main
