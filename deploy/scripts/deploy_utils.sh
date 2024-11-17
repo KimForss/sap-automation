38101285cec95bd68e08fec4f088fc49cc48cef4
Kimmo Forss <kimforss@microsoft.com>
2024-11-17 07:27:09 +0200

    Refactor variable value retrieval in load_config_vars function to improve whitespace handling

--- deploy/scripts/deploy_utils.sh
+++ deploy/scripts/deploy_utils.sh
@@ -47,10 +47,10 @@ function load_config_vars() {
   fi
   for var_name; do # iterate over function params
     # NOTE: Should we care if we fail to retrieve a value from the file?
-    var_value="$(grep -m1 "^${var_name}=" "${var_file}" | cut -d'=' -f2- | tr -d ' ' | tr -d '"')"
+    var_value="$(grep -m1 "^${var_name}=" "${var_file}" | cut -d'=' -f2- | tr ' \t\n\r\f"')"
 
     if [ -z "${var_value}" ]; then
-      var_value="$(grep -m1 "^${var_name}[[:space:]]=" "${var_file}" | cut -d'=' -f2- | tr -d ' ' | tr -d '"')"
+      var_value="$(grep -m1 "^${var_name}[[:space:]]=" "${var_file}" | cut -d'=' -f2- | tr -d ' \t\n\r\f"')"
     fi
 
     # NOTE: this continue means we skip setting an empty value for a variable
