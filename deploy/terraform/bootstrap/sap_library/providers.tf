4c53c39295fae9712baebefb148384e78ea13d68
Kimmo Forss <kimforss@microsoft.com>
2024-11-19 19:10:08 +0200

    Update subscription_id assignment in providers.tf to use coalesce for improved handling

--- deploy/terraform/bootstrap/sap_library/providers.tf
+++ deploy/terraform/bootstrap/sap_library/providers.tf
@@ -63,7 +63,13 @@ provider "azurerm"                     {
 
                                          storage_use_azuread        = true
                                          use_msi                    = false
-                                         subscription_id            = var.use_deployer ? data.terraform_remote_state.deployer[0].outputs.created_resource_group_subscription_id : null
+                                         subscription_id            = var.use_deployer ? (
+                                                                        coalesce(
+                                                                          data.azurerm_client_config.current.subscription_id ,
+                                                                          data.terraform_remote_state.deployer[0].outputs.created_resource_group_subscription_id)
+                                                                          ) : (
+                                                                        null
+                                                                        )
                                        }
 
 provider "azurerm"                     {
