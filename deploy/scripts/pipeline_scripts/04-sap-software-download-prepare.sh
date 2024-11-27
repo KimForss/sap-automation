c5d74acd0be21a1a8020c7ae8be7d3f4db5a9c3c
Kimmo Forss <kimforss@microsoft.com>
2024-11-27 15:46:58 +0200

    Fix variable group name reference in SAP software download preparation script

--- deploy/scripts/pipeline_scripts/04-sap-software-download-prepare.sh
+++ deploy/scripts/pipeline_scripts/04-sap-software-download-prepare.sh
@@ -69,12 +69,12 @@ fi
 az account set --subscription "$ARM_SUBSCRIPTION_ID" --output none
 
 echo -e "$green--- Get key_vault name ---$reset"
-VARIABLE_GROUP_ID=$(az pipelines variable-group list --query "[?name=='$(variable_group)'].id | [0]")
+VARIABLE_GROUP_ID=$(az pipelines variable-group list --query "[?name=='$VARIABLE_GROUP'].id | [0]")
 export VARIABLE_GROUP_ID
 printf -v val '%-15s' "$(variable_group) id:"
 echo "$val                      $VARIABLE_GROUP_ID"
 if [ -z "${VARIABLE_GROUP_ID}" ]; then
-  echo "##vso[task.logissue type=error]Variable group $(variable_group) could not be found."
+  echo "##vso[task.logissue type=error]Variable group $VARIABLE_GROUP could not be found."
   exit 2
 fi
 
