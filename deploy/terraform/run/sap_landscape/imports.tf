2090f4a219dc4b656db6ed5b4f82d8ec720ff578
Kimmo Forss <kimforss@microsoft.com>
2024-12-03 12:50:15 +0200

    Add conditional count for azurerm_key_vault_secret based on use_spn variable

--- deploy/terraform/run/sap_landscape/imports.tf
+++ deploy/terraform/run/sap_landscape/imports.tf
@@ -21,6 +21,7 @@ data "terraform_remote_state" "deployer" {
 }
 
 data "azurerm_key_vault_secret" "subscription_id" {
+  count                                = var.use_spn ? 1 : 0
   name                                 = format("%s-subscription-id", local.environment)
   key_vault_id                         = local.spn_key_vault_arm_id
   timeouts                             {
