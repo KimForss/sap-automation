4e438876487c55c8ace27cd2e669048f8c4489d9
Kimmo Forss <kimforss@microsoft.com>
2025-04-02 12:25:29 +0300

    Fix count logic in subnet data imports to ensure proper existence checks

--- deploy/terraform/terraform-units/modules/sap_system/common_infrastructure/subnets.tf
+++ deploy/terraform/terraform-units/modules/sap_system/common_infrastructure/subnets.tf
@@ -56,7 +56,7 @@ resource "azurerm_subnet" "db" {
 // Imports data of existing db subnet
 data "azurerm_subnet" "db" {
   provider                             = azurerm.main
-  count                                = (var.infrastructure.virtual_networks.sap.subnet_db.exists || var.infrastructure.virtual_networks.sap.subnet_db.exists_in_workload )
+  count                                = (var.infrastructure.virtual_networks.sap.subnet_db.exists || var.infrastructure.virtual_networks.sap.subnet_db.exists_in_workload ) ? 1 : 0
   name                                 = split("/", coalesce(var.infrastructure.virtual_networks.sap.subnet_db.id, var.infrastructure.virtual_networks.sap.subnet_db.id_in_workload))[10]
   resource_group_name                  = split("/", coalesce(var.infrastructure.virtual_networks.sap.subnet_db.id, var.infrastructure.virtual_networks.sap.subnet_db.id_in_workload))[4]
   virtual_network_name                 = split("/", coalesce(var.infrastructure.virtual_networks.sap.subnet_db.id, var.infrastructure.virtual_networks.sap.subnet_db.id_in_workload))[8]
@@ -93,7 +93,7 @@ resource "azurerm_subnet" "storage" {
 // Imports data of existing db subnet
 data "azurerm_subnet" "storage" {
   provider                             = azurerm.main
-  count                                = (var.infrastructure.virtual_networks.sap.subnet_storage.exists || var.infrastructure.virtual_networks.sap.subnet_storage.exists_in_workload )
+  count                                = (var.infrastructure.virtual_networks.sap.subnet_storage.exists || var.infrastructure.virtual_networks.sap.subnet_storage.exists_in_workload) ? 1 : 0
   name                                 = split("/", coalesce(var.infrastructure.virtual_networks.sap.subnet_storage.id, var.infrastructure.virtual_networks.sap.subnet_storage.id_in_workload))[10]
   resource_group_name                  = split("/", coalesce(var.infrastructure.virtual_networks.sap.subnet_storage.id, var.infrastructure.virtual_networks.sap.subnet_storage.id_in_workload))[4]
   virtual_network_name                 = split("/", coalesce(var.infrastructure.virtual_networks.sap.subnet_storage.id, var.infrastructure.virtual_networks.sap.subnet_storage.id_in_workload))[8]
