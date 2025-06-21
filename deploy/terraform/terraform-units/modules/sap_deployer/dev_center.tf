cf16f5c8d39637f73ac4fd993f3771d4368c6b26
Kimmo Forss <kimforss@microsoft.com>
2025-06-21 18:42:47 +0300

    fix: Update image_reference_id in dev center dev box definition to use correct dev center ID

--- deploy/terraform/terraform-units/modules/sap_deployer/dev_center.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/dev_center.tf
@@ -56,7 +56,7 @@ resource "azurerm_dev_center_dev_box_definition" "deployer" {
   name                                          = "SDAF"
   location                                      = var.infrastructure.resource_group.exists ? data.azurerm_resource_group.deployer[0].location : azurerm_resource_group.deployer[0].location
   dev_center_id                                 = azurerm_dev_center.deployer[0].id
-  image_reference_id                            = "${azurerm_dev_center.example.id}/galleries/default/images/microsoftvisualstudio_visualstudioplustools_vs-2022-ent-general-win10-m365-gen2"
+  image_reference_id                            = "${azurerm_dev_center.deployer[0].id}/galleries/default/images/microsoftvisualstudio_visualstudioplustools_vs-2022-ent-general-win10-m365-gen2"
   sku_name                                      = "general_i_8c32gb256ssd_v2"
 }
 
