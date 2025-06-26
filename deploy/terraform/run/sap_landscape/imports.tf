cd099bfdf5aa6d14a0e52f21058e7d3fa3cab0fc
Kimmo Forss <kimforss@microsoft.com>
2025-06-26 18:31:04 +0300

    Refactor Terraform configuration for SAP landscape
    - Simplified local variable definitions for subnet configurations by removing redundant checks and consolidating logic.
    - Updated resource group definition to use 'id' instead of 'arm_id'.
    - Renamed variables for consistency, changing 'arm_id' to 'id' across various resources.
    - Enhanced subnet definitions to include 'exists' checks for better validation.
    - Improved key vault configuration by merging temporary and specified key vault settings.
    - Cleaned up commented-out code related to service principal and account definitions for clarity.
    - Corrected spelling in comments and variable names for better readability.

--- deploy/terraform/run/sap_landscape/imports.tf
+++ deploy/terraform/run/sap_landscape/imports.tf
@@ -9,16 +9,21 @@
 
 data "azurerm_client_config" "current" {}
 
+data "azurerm_client_config" "current_main" {
+  provider                            = azurerm.workload
+
+}
+
 data "terraform_remote_state" "deployer" {
   backend                              = "azurerm"
 
-  count                                = length(try(var.deployer_tfstate_key, "")) > 0 ? 1 : 0
+  count                                = var.use_deployer && length(try(var.deployer_tfstate_key, "")) > 0 ? 1 : 0
   config                               = {
-                                           resource_group_name  = local.saplib_resource_group_name
+                                           resource_group_name  = local.SAPLibrary_resource_group_name
                                            storage_account_name = local.tfstate_storage_account_name
                                            container_name       = local.tfstate_container_name
                                            key                  = trimspace(var.deployer_tfstate_key)
-                                           subscription_id      = local.saplib_subscription_id
+                                           subscription_id      = local.SAPLibrary_subscription_id
 
                                          }
 }
@@ -41,13 +46,11 @@ data "azurerm_key_vault_secret" "client_id" {
                                        }
 }
 
-data "azurerm_key_vault_secret" "client_secret" {
+ephemeral "azurerm_key_vault_secret" "client_secret" {
   count                                = var.use_spn ? 1 : 0
   name                                 = format("%s-client-secret", local.environment)
   key_vault_id                         = local.spn_key_vault_arm_id
-  timeouts                             {
-                                          read = "1m"
-                                       }
+
 }
 
 data "azurerm_key_vault_secret" "tenant_id" {
@@ -70,33 +73,20 @@ data "azurerm_key_vault_secret" "cp_subscription_id" {
 
 data "azurerm_key_vault_secret" "cp_client_id" {
   count                                = var.use_spn ? 1 : 0
-  name                                 = format("%s-client-id", data.terraform_remote_state.deployer[0].outputs.environment)
+  name                                 = format("%s-client-id", data.terraform_remote_state.deployer[0].outputs.control_plane_name)
   key_vault_id                         = local.spn_key_vault_arm_id
-  timeouts                             {
-                                          read = "1m"
-                                       }
+
 }
 
-data "azurerm_key_vault_secret" "cp_client_secret" {
+ephemeral "azurerm_key_vault_secret" "cp_client_secret" {
   count                                = var.use_spn ? 1 : 0
-  name                                 = format("%s-client-secret", data.terraform_remote_state.deployer[0].outputs.environment)
+  name                                 = format("%s-client-secret", data.terraform_remote_state.deployer[0].outputs.control_plane_name)
   key_vault_id                         = local.spn_key_vault_arm_id
-  timeouts                             {
-                                          read = "1m"
-                                       }
 }
 
 data "azurerm_key_vault_secret" "cp_tenant_id" {
   count                                = var.use_spn ? 1 : 0
-  name                                 = format("%s-tenant-id", data.terraform_remote_state.deployer[0].outputs.environment)
+  name                                 = format("%s-tenant-id", data.terraform_remote_state.deployer[0].outputs.control_plane_name)
   key_vault_id                         = local.spn_key_vault_arm_id
-  timeouts                             {
-                                          read = "1m"
-                                       }
 }
 
-// Import current service principal
-data "azuread_service_principal" "sp"             {
-                                                    count          = var.use_spn ? 1 : 0
-                                                    client_id      = local.spn.client_id
-                                                  }
