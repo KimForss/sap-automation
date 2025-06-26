6b7da4d0d2f1d7c08e5dd400a73a31c2850b7cd3
Kimmo Forss <kimforss@microsoft.com>
2025-06-27 02:36:49 +0300

    refactor: Update script path handling and improve readability in SAP deployment script

--- deploy/scripts/pipeline_scripts/v1/04-sap-software-download-prepare.sh
+++ deploy/scripts/pipeline_scripts/v1/04-sap-software-download-prepare.sh
@@ -5,8 +5,24 @@
 green="\e[1;32m"
 reset="\e[0m"
 bold_red="\e[1;31m"
+
+
 #External helper functions
-source "sap-automation/deploy/pipelines/helper.sh"
+full_script_path="$(realpath "${BASH_SOURCE[0]}")"
+script_directory="$(dirname "${full_script_path}")"
+parent_directory="$(dirname "$script_directory")"
+grand_parent_directory="$(dirname "$parent_directory")"
+
+SCRIPT_NAME="$(basename "$0")"
+
+banner_title="Deploy SAP System"
+
+#call stack has full script name when using source
+# shellcheck disable=SC1091
+source "${grand_parent_directory}/deploy_utils.sh"
+
+#call stack has full script name when using source
+source "${parent_directory}/helper.sh"
 
 DEBUG=False
 
