dae625b2af0a750a8d874e612291bdf287c3b101
Kimmo Forss <kimforss@microsoft.com>
2025-06-27 03:04:13 +0300

    refactor: Improve script structure and Azure login handling in SAP software download script

--- deploy/scripts/pipeline_scripts/04-sap-software-download.sh
+++ deploy/scripts/pipeline_scripts/04-sap-software-download.sh
@@ -5,8 +5,24 @@
 green="\e[1;32m"
 reset="\e[0m"
 bold_red="\e[1;31m"
+
+
 #External helper functions
-source "sap-automation/deploy/pipelines/helper.sh"
+full_script_path="$(realpath "${BASH_SOURCE[0]}")"
+script_directory="$(dirname "${full_script_path}")"
+parent_directory="$(dirname "$script_directory")"
+grand_parent_directory="$(dirname "$parent_directory")"
+
+SCRIPT_NAME="$(basename "$0")"
+
+banner_title="Deploy SAP System"
+
+#call stack has full script name when using source
+# shellcheck disable=SC1091
+source "${grand_parent_directory}/deploy_utils.sh"
+
+#call stack has full script name when using source
+source "${parent_directory}/helper.sh"
 
 DEBUG=False
 
@@ -41,22 +57,30 @@ if [ "$USE_MSI" != "true" ]; then
   fi
 fi
 
-echo -e "$green--- az login ---$reset"
-# Check if running on deployer
-if [ ! -f /etc/profile.d/deploy_server.sh ]; then
-  echo -e "$green--- az login ---$reset"
-  LogonToAzure false
+# Set logon variables
+if [ $USE_MSI == "true" ]; then
+	unset ARM_CLIENT_SECRET
+	ARM_USE_MSI=true
+	export ARM_USE_MSI
+fi
+if az account show --query name; then
+	echo -e "$green--- Already logged in to Azure ---$reset"
 else
-  LogonToAzure "$USE_MSI"
+	# Check if running on deployer
+	if [[ ! -f /etc/profile.d/deploy_server.sh ]]; then
+		configureNonDeployer "${tf_version:-1.12.2}"
+		echo -e "$green--- az login ---$reset"
+		LogonToAzure $USE_MSI
+	else
+		LogonToAzure $USE_MSI
+	fi
+	return_code=$?
+	if [ 0 != $return_code ]; then
+		echo -e "$bold_red--- Login failed ---$reset"
+		echo "##vso[task.logissue type=error]az login failed."
+		exit $return_code
+	fi
 fi
-return_code=$?
-
-if [ 0 != $return_code ]; then
-  echo -e "$bold_red--- Login failed ---$reset"
-  echo "##vso[task.logissue type=error]az login failed."
-  exit $return_code
-fi
-
 az account set --subscription "$ARM_SUBSCRIPTION_ID" --output none
 
 command="ansible-playbook -e download_directory=$AGENT_TEMP_DIRECTORY \
