42df1536341560b334b12b5f2c190d2fc27a1d99
Kimmo Forss <kimforss@microsoft.com>
2025-09-29 20:02:25 +0300

    feat: add client ID environment variables for Azure Storage Table and Blob connections

--- deploy/terraform/terraform-units/modules/sap_deployer/app_service.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/app_service.tf
@@ -130,6 +130,9 @@ resource "azurerm_windows_web_app" "webapp" {
                                                   )) : (
                                                   ""
                                                  )
+
+    "AZURE_STORAGETABLE_CLIENTID"                = length(var.deployer.user_assigned_identity_id) > 0 ? data.azurerm_user_assigned_identity.deployer[0].client_id : azurerm_user_assigned_identity.deployer[0].client_id
+    "AZURE_STORAGEBLOB_CLIENTID"                  = length(var.deployer.user_assigned_identity_id) > 0 ? data.azurerm_user_assigned_identity.deployer[0].client_id : azurerm_user_assigned_identity.deployer[0].client_id
   }
 
   sticky_settings {
