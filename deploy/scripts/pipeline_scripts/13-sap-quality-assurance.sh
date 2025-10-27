10f1d041511d53980e4d4d74207c3c9790324e20
hdamecharla <hdamecharla@microsoft.com>
2025-10-27 17:36:31 +0530

    fix: enhance SAP Quality Assurance script with improved variable handling and error logging

--- deploy/scripts/pipeline_scripts/13-sap-quality-assurance.sh
+++ deploy/scripts/pipeline_scripts/13-sap-quality-assurance.sh
@@ -2,11 +2,29 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+echo "##vso[build.updatebuildnumber]Deploying the control plane defined in $DEPLOYER_FOLDERNAME $LIBRARY_FOLDERNAME"
 green="\e[1;32m"
 reset="\e[0m"
 bold_red="\e[1;31m"
-#External helper functions
-source "sap-automation/deploy/pipelines/helper.sh"
+cyan="\e[1;36m"
+
+# External helper functions
+#. "$(dirname "${BASH_SOURCE[0]}")/deploy_utils.sh"
+full_script_path="$(realpath "${BASH_SOURCE[0]}")"
+script_directory="$(dirname "${full_script_path}")"
+parent_directory="$(dirname "$script_directory")"
+grand_parent_directory="$(dirname "$parent_directory")"
+
+SCRIPT_NAME="$(basename "$0")"
+
+banner_title="SAP Configuration and Installation"
+
+#call stack has full script name when using source
+# shellcheck disable=SC1091
+source "${grand_parent_directory}/deploy_utils.sh"
+
+#call stack has full script name when using source
+source "${parent_directory}/helper.sh"
 
 DEBUG=False
 
@@ -20,32 +38,40 @@ fi
 export DEBUG
 set -eu
 
-echo -e "$green--- Configure devops CLI extension ---$reset"
-az config set extension.use_dynamic_install=yes_without_prompt --output none --only-show-errors
-AZURE_DEVOPS_EXT_PAT=$SYSTEM_ACCESSTOKEN
-export AZURE_DEVOPS_EXT_PAT
+# Print the execution environment details
+print_header
 
-ENVIRONMENT=$(echo "$SAP_SYSTEM_CONFIGURATION_NAME" | awk -F'-' '{print $1}' | xargs)
+# Configure DevOps
+configure_devops
 
-LOCATION=$(echo "${SAP_SYSTEM_CONFIGURATION_NAME}" | awk -F'-' '{print $2}' | xargs)
+if ! get_variable_group_id "$VARIABLE_GROUP" "VARIABLE_GROUP_ID" ;
+then
+	echo -e "$bold_red--- Variable group $VARIABLE_GROUP not found ---$reset"
+	echo "##vso[task.logissue type=error]Variable group $VARIABLE_GROUP not found."
+	exit 2
+fi
+export VARIABLE_GROUP_ID
 
-NETWORK=$(echo "${SAP_SYSTEM_CONFIGURATION_NAME}" | awk -F'-' '{print $3}' | xargs)
+WORKLOAD_ZONE_NAME=$(basename "${SAP_SYSTEM_CONFIGURATION_NAME}" | cut -d'-' -f1-3)
+SID=$(basename "${SAP_SYSTEM_CONFIGURATION_NAME}" | cut -d'-' -f4)
 
-SID=$(echo "${SAP_SYSTEM_CONFIGURATION_NAME}" | awk -F'-' '{print $4}' | xargs)
+print_banner "$banner_title" "Starting $SCRIPT_NAME" "info"
+
+if is_valid_id "$APPLICATION_CONFIGURATION_ID" "/providers/Microsoft.AppConfiguration/configurationStores/"; then
+
+	key_vault_id=$(getVariableFromApplicationConfiguration "$APPLICATION_CONFIGURATION_ID" "${WORKLOAD_ZONE_NAME}_KeyVaultResourceId" "${WORKLOAD_ZONE_NAME}")
+	key_vault=$(echo "$key_vault_id" | cut -d'/' -f9)
+	key_vault_subscription_id=$(echo "$key_vault_id" | cut -d'/' -f3)
+
+	tfstate_resource_id=$(getVariableFromApplicationConfiguration "$APPLICATION_CONFIGURATION_ID" "${CONTROL_PLANE_NAME}_TerraformRemoteStateStorageAccountId" "${CONTROL_PLANE_NAME}")
+	tfstate_subscription_id=$(echo "$tfstate_resource_id" | cut -d'/' -f3)
+fi
 
 cd "$CONFIG_REPO_PATH" || exit
 
-environment_file_name=".sap_deployment_automation/$ENVIRONMENT$LOCATION$NETWORK"
 parameters_filename="$CONFIG_REPO_PATH/SYSTEM/${SAP_SYSTEM_CONFIGURATION_NAME}/sap-parameters.yaml"
 
-az devops configure --defaults organization=$SYSTEM_COLLECTIONURI project='$SYSTEM_TEAMPROJECT' --output none --only-show-errors
-
 echo -e "$green--- Validations ---$reset"
-if [ ! -f "${environment_file_name}" ]; then
-	echo -e "$bold_red--- ${environment_file_name} was not found ---$reset"
-	echo "##vso[task.logissue type=error]File ${environment_file_name} was not found."
-	exit 2
-fi
 
 if [ -z "$AZURE_SUBSCRIPTION_ID" ]; then
 	echo "##vso[task.logissue type=error]Variable AZURE_SUBSCRIPTION_ID was not defined."
@@ -57,75 +83,36 @@ if [ "azure pipelines" == "$THIS_AGENT" ]; then
 	exit 2
 fi
 
-echo -e "$green--- az login ---$reset"
 # Check if running on deployer
 if [[ ! -f /etc/profile.d/deploy_server.sh ]]; then
-	#configureNonDeployer "${tf_version:-1.12.2}"
+	configureNonDeployer "$(tf_version)"
 	echo -e "$green--- az login ---$reset"
-	LogonToAzure "$USE_MSI"
-else
-	LogonToAzure "$USE_MSI"
-fi
-return_code=$?
-if [ 0 != $return_code ]; then
-	echo -e "$bold_red--- Login failed ---$reset"
-	echo "##vso[task.logissue type=error]az login failed."
-	exit $return_code
+	if ! LogonToAzure false; then
+		print_banner "$banner_title" "Login to Azure failed" "error"
+		echo "##vso[task.logissue type=error]az login failed."
+		exit 2
+	fi
 fi
 
-az account set --subscription "$AZURE_SUBSCRIPTION_ID" --output none
-
-echo -e "$green--- Get key_vault name ---$reset"
-VARIABLE_GROUP_ID=$(az pipelines variable-group list --query "[?name=='$VARIABLE_GROUP'].id | [0]")
-export VARIABLE_GROUP_ID
-printf -v val '%-15s' "$VARIABLE_GROUP_ID id:"
-echo "$val                      $VARIABLE_GROUP_ID"
-if [ -z "${VARIABLE_GROUP_ID}" ]; then
-	echo "##vso[task.logissue type=error]Variable group $VARIABLE_GROUP could not be found."
-	exit 2
-fi
-
-key_vault=$(getVariableFromVariableGroup "${VARIABLE_GROUP_ID}" "Deployer_Key_Vault" "${environment_file_name}" "keyvault")
-
-
-echo "##vso[task.setvariable variable=SID;isOutput=true]${SID}"
-echo "##vso[task.setvariable variable=SAP_PARAMETERS;isOutput=true]sap-parameters.yaml"
-echo "##vso[task.setvariable variable=FOLDER;isOutput=true]$CONFIG_REPO_PATH/SYSTEM/$SAP_SYSTEM_CONFIGURATION_NAME"
-echo "##vso[task.setvariable variable=HOSTS;isOutput=true]${SID}_hosts.yaml"
-echo "##vso[task.setvariable variable=KV_NAME;isOutput=true]$key_vault"
-
-echo "Environment:                         $ENVIRONMENT"
-echo "Location:                            $LOCATION"
-echo "Virtual network logical name:        $NETWORK"
-echo "Keyvault:                            $key_vault"
+echo "##vso[build.updatebuildnumber]Deploying ${SAP_SYSTEM_CONFIGURATION_NAME} using BoM ${BOM_BASE_NAME}"
 
 echo "SID:                                 ${SID}"
+echo "Workload Zone Name:                  $WORKLOAD_ZONE_NAME"
+echo "Keyvault:                            $key_vault"
+echo "SAP Application BoM:                 $BOM_BASE_NAME"
+
 echo "Folder:                              $CONFIG_REPO_PATH/SYSTEM/${SAP_SYSTEM_CONFIGURATION_NAME}"
 echo "Hosts file:                          ${SID}_hosts.yaml"
 echo "sap_parameters_file:                 $parameters_filename"
-echo "Configuration file:                  $environment_file_name"
 
-echo -e "$green--- Get Files from the DevOps Repository ---$reset"
 cd "$CONFIG_REPO_PATH/SYSTEM/${SAP_SYSTEM_CONFIGURATION_NAME}"
 
-echo -e "$green--- Get connection details ---$reset"
+echo -e "$green--- Add BOM Base Name and SAP FQDN to sap-parameters.yaml ---$reset"
+sed -i 's|bom_base_name:.*|bom_base_name:                 '"$BOM_BASE_NAME"'|' sap-parameters.yaml
+
 mkdir -p artifacts
 
-prefix="${ENVIRONMENT}${LOCATION}${NETWORK}"
-echo "Prefix:                              ${prefix}"
-echo "Looking for variable: ${prefix}Workload_Key_Vault"
-echo "Looking for variable: ${prefix}Workload_Secret_Prefix"
-az pipelines variable-group variable list --group-id "${VARIABLE_GROUP_ID}" --output table --only-show-errors
-workload_key_vault=$(getVariableFromVariableGroup "${VARIABLE_GROUP_ID}" "${prefix}Workload_Key_Vault" "${environment_file_name}" "workloadkeyvault" || true)
-workload_prefix=$(getVariableFromVariableGroup "${VARIABLE_GROUP_ID}" "${prefix}Workload_Secret_Prefix" "${environment_file_name}" "workload_zone_prefix" || true)
-control_plane_subscription=$(getVariableFromVariableGroup "${VARIABLE_GROUP_ID}" "Terraform_Remote_Storage_Subscription" "${environment_file_name}" "STATE_SUBSCRIPTION" || true)
-
-echo "SID:                                 ${SID}"
-echo "Folder:                              $HOME/SYSTEM/${SAP_SYSTEM_CONFIGURATION_NAME}"
 echo "Workload Key Vault:                  ${key_vault}"
-
-echo "Control Plane Subscription:          ${control_plane_subscription}"
-echo "Workload Prefix:                     ${workload_prefix}"
 
 if [ $EXTRA_PARAMETERS = '$(EXTRA_PARAMETERS)' ]; then
 	new_parameters=$PIPELINE_EXTRA_PARAMETERS
@@ -134,18 +121,26 @@ else
 	new_parameters="$EXTRA_PARAMETERS $PIPELINE_EXTRA_PARAMETERS"
 fi
 
-echo "##vso[task.setvariable variable=SSH_KEY_NAME;isOutput=true]${workload_prefix}-sid-sshkey"
-echo "##vso[task.setvariable variable=VAULT_NAME;isOutput=true]$workload_key_vault"
-echo "##vso[task.setvariable variable=PASSWORD_KEY_NAME;isOutput=true]${workload_prefix}-sid-password"
-echo "##vso[task.setvariable variable=USERNAME_KEY_NAME;isOutput=true]${workload_prefix}-sid-username"
-echo "##vso[task.setvariable variable=NEW_PARAMETERS;isOutput=true]${new_parameters}"
-echo "##vso[task.setvariable variable=CP_SUBSCRIPTION;isOutput=true]${control_plane_subscription}"
+az account set --subscription "$tfstate_subscription_id" --output none --only-show-errors
 
-az keyvault secret show --name "${workload_prefix}-sid-sshkey" --vault-name "${key_vault}" --subscription "$control_plane_subscription" --query value -o tsv >"artifacts/${SAP_SYSTEM_CONFIGURATION_NAME}_sshkey"
+echo "##vso[task.setvariable variable=CP_SUBSCRIPTION;isOutput=true]${tfstate_subscription_id}"
+echo "##vso[task.setvariable variable=FOLDER;isOutput=true]$CONFIG_REPO_PATH/SYSTEM/$SAP_SYSTEM_CONFIGURATION_NAME"
+echo "##vso[task.setvariable variable=HOSTS;isOutput=true]${SID}_hosts.yaml"
+echo "##vso[task.setvariable variable=KV_NAME;isOutput=true]$key_vault"
+echo "##vso[task.setvariable variable=NEW_PARAMETERS;isOutput=true]${new_parameters}"
+echo "##vso[task.setvariable variable=PASSWORD_KEY_NAME;isOutput=true]${WORKLOAD_ZONE_NAME}-sid-password"
+echo "##vso[task.setvariable variable=SAP_PARAMETERS;isOutput=true]sap-parameters.yaml"
+echo "##vso[task.setvariable variable=SID;isOutput=true]${SID}"
+echo "##vso[task.setvariable variable=SSH_KEY_NAME;isOutput=true]${WORKLOAD_ZONE_NAME}-sid-sshkey"
+echo "##vso[task.setvariable variable=USERNAME_KEY_NAME;isOutput=true]${WORKLOAD_ZONE_NAME}-sid-username"
+echo "##vso[task.setvariable variable=VAULT_NAME;isOutput=true]$key_vault"
+
+az keyvault secret show --name "${WORKLOAD_ZONE_NAME}-sid-sshkey" --vault-name "$key_vault" --subscription "$key_vault_subscription_id" --query value -o tsv >"artifacts/${SAP_SYSTEM_CONFIGURATION_NAME}_sshkey"
 cp sap-parameters.yaml artifacts/.
 cp "${SID}_hosts.yaml" artifacts/.
 
 2> >(while read line; do (echo >&2 "STDERROR: $line"); done)
 
 echo -e "$green--- Done ---$reset"
+print_banner "$banner_title" "Exiting $SCRIPT_NAME" "info"
 exit 0
