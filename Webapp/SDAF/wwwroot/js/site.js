b56cd884cc04431a06845a9e553a2bfd26203f55
Kimmo Forss <kimforss@microsoft.com>
2026-05-15 14:40:25 +0300

    Web App Updates (WIP)

--- Webapp/SDAF/wwwroot/js/site.js
+++ Webapp/SDAF/wwwroot/js/site.js
@@ -764,3 +764,27 @@ function filterResults(searchText) {
 jQuery.expr[':'].Contains = function (a, i, m) {
     return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
 };
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const notifications = document.querySelectorAll('.notificationContainer[data-timeout]');
+
+  notifications.forEach(notification => {
+    const timeout = parseInt(notification.getAttribute('data-timeout')) || 5000;
+
+    // Close button handler
+    const closeBtn = notification.querySelector('.close');
+    if (closeBtn) {
+      closeBtn.addEventListener('click', () => {
+        notification.style.opacity = '0';
+        setTimeout(() => notification.remove(), 300);
+      });
+    }
+
+    // Auto-dismiss
+    setTimeout(() => {
+      notification.style.opacity = '0';
+      setTimeout(() => notification.remove(), 300);
+    }, timeout);
+  });
+});
